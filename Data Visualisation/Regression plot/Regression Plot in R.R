d078b7f28ceb967b230674a5a8cebdf06faef6ce
souhassous <96495298+souhassous@users.noreply.github.com>
2022-02-26 19:27:53 +0100

    Update Regression Plot in R.R

--- Data Visualisation/Regression plot/Regression Plot in R.R
+++ Data Visualisation/Regression plot/Regression Plot in R.R
@@ -1,3 +1,18 @@
 library(ggplot2)
 
-df <- read.csv("sc")
+df <- read.csv("scatter_plot_ii.csv",
+               header = TRUE,
+               sep = ",")
+df
+regPlot <- ggplot(data = df,
+                  aes(x = Budget,y = Sales)) +
+  geom_point(size = 3,
+             color = "grey12") +
+  geom_smooth(method = "lm",
+              color = "red",
+              fill = "red") +
+  theme_classic() + 
+  xlab("Ad Expenditure (K$)") + 
+  ylab("Sales (KUnits)") + 
+  ggtitle("Effect of Ad Expenditure on Sales")
+regPlot
